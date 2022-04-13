d11e1510f536c519a4d91c847795555efdabbe7f
Christian Hemefor <hemeforc@gmail.com>
2022-04-13 11:43:03 -0600

    Functional Component

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,8 +1,8 @@
-import { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardBody, CardTitle} from 'reactstrap';
 
-class CampsiteInfo extends Component{
-    renderCampsite (campsite) {
+
+    function RenderCampsite ({campsite}) {
         return(
             <div className=" col-md-5 m-1">
                 <Card>
@@ -15,7 +15,7 @@ class CampsiteInfo extends Component{
             </div>
         )
     }
-    renderComments(comments) {
+    function RenderComments({comments}) {
         if (comments) {
             return (
                 <div className="col-md-5 m-1">
@@ -32,13 +32,13 @@ class CampsiteInfo extends Component{
 
         }return <div/>
     }
-render (){
-if ( this.props.campsite ) {
+function CampsiteInfo(props){
+if ( props.campsite ) {
  return ( 
     <div className="container">
  <div className="row">
-     {this.renderCampsite(this.props.campsite)}
-     {this.renderComments(this.props.campsite.comments)}
+     <RenderCampsite campsite={props.campsite} />
+     <RenderComments comments={props.campsite.comments} />
     </div> 
     </div>
  );
@@ -46,6 +46,6 @@ if ( this.props.campsite ) {
  
 return <div/>
 }
-}
+
 
 export default CampsiteInfo;
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import { CAMPSITES } from '../shared/campsites';
 
-class Directory extends Component {   
-    render() {
-        const directory = this.props.campsites.map(campsite => {
+
+function RenderDirectoryItem({campsite,onClick}) {
+    return (
+       <Card onClick={() => onClick(campsite.id)}>
+          <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+          <CardImgOverlay>
+          <CardTitle>{campsite.name}</CardTitle>
+           </CardImgOverlay>
+       </Card>
+    );
+}
+function Directory(props) {   
+    
+        const directory = props.campsites.map(campsite => {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(campsite.id)}>
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+                   <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
                 </div>
             );
         });
@@ -26,6 +31,6 @@ class Directory extends Component {
         );
     }
     
-}
+
 
 export default Directory;
