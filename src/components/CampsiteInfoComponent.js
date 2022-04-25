851a317a6785c08526bb6c5dc2e711a507996061
Christian Hemefor <hemeforc@gmail.com>
2022-04-25 13:53:25 -0600

    Redux Thunk.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -6,6 +6,7 @@ import Button from "reactstrap/lib/Button";
 import Modal from "reactstrap/lib/Modal";
 import ModalHeader from "reactstrap/lib/ModalHeader";
 import { Control,Errors, LocalForm } from "react-redux-form";
+import { Loading } from './LoadingComponent';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -43,6 +44,26 @@ const isNumber = val => !isNaN(+val);
         }return <div/>
     }
 function CampsiteInfo(props){
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 if ( props.campsite ) {
     return (
         <div className="container">
