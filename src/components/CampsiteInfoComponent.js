f1f4711ca069a3f695c8c3ad16d9d3d59a18d704
Christian Hemefor <hemeforc@gmail.com>
2022-04-25 11:54:07 -0600

    Combining Reducers

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,16 @@
-import React from "react";
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Label, ModalBody, } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { render } from "@testing-library/react";
+import Button from "reactstrap/lib/Button";
+import Modal from "reactstrap/lib/Modal";
+import ModalHeader from "reactstrap/lib/ModalHeader";
+import { Control,Errors, LocalForm } from "react-redux-form";
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+const isNumber = val => !isNaN(+val);
 
     function RenderCampsite ({campsite}) {
         return(
@@ -25,7 +35,7 @@ import { Link } from 'react-router-dom';
                     <p> {comment.author},
                     {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                     </div> )}
-                    
+                    <CommentForm/>
                 </div>
             )
 
@@ -55,6 +65,83 @@ if ( props.campsite ) {
  
 return <div/>
 }
+
+// Comment Form
+class CommentForm extends Component {
+    constructor (props) {
+        super(props)
+        this.state ={
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+toggleModal() {
+    this.setState({
+        isModalOpen: !this.state.isModalOpen
+    });
+}
+handleSubmit(values) {
+    console.log('Current state is: ' + JSON.stringify(values));
+    alert('Current state is: ' + JSON.stringify(values));
+}
+
+
+
+render() {
+    return(
+<div className="container" >
+<Button outline online onClick ={this.toggleModal}>
+    <i className="fa fa-pencil fa-lg"/>
+    Submit Comment
+</Button>
+<Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+    <ModalBody>
+<LocalForm onSubmit={values => this.handleSubmit(values)}>
+<div className="form-group">
+    <Label htmlfor="rating">Rating</Label>
+    <Control.select model=".rating" id ="rating" name="rating" className="form-control">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+        </Control.select>
+</div>
+<div className="form-group">
+<Label htmlfor="author">Your Name</Label>
+    <Control.text model=".author" id ="author" name="author" className="form-control" 
+    validators={{
+        minLength: minLength(2),
+        maxLength: maxLength(15)
+    }}
+    />
+    <Errors 
+    className="text-danger"
+    model=".author"
+    show="touched"
+    component="div"
+    messages={{
+        required: 'Required',
+        minLength: 'Must be at least 2 characters',
+        maxLength: 'Must be 15 characters or less'
+    }}
+/>
+</div>
+<div className="form-group row-6">
+<Label htmlfor="text">Comment</Label>
+    <Control.textarea model=".text" id ="text" name="text" className="form-control"/>
+    </div>
+    <Button type="summit" color="primary">Summit</Button>
+</LocalForm>
+    </ModalBody>
+</Modal>
+</div>
+    );
+}
+
+}
+
 
 
 export default CampsiteInfo;
