f6e862114db3f987a121f5400e2d4b52f27c5c98
Christian Hemefor <hemeforc@gmail.com>
2022-04-26 15:55:16 -0600

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -26,7 +26,7 @@ const isNumber = val => !isNaN(+val);
             </div>
         )
     }
-    function RenderComments({comments, addComment, campsiteId}) {
+    function RenderComments({comments, postComment, campsiteId}) {
         if (comments) {
             return (
                 <div className="col-md-5 m-1">
@@ -37,7 +37,7 @@ const isNumber = val => !isNaN(+val);
                     <p> {comment.author},
                     {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                     </div> )}
-                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                    <CommentForm campsiteId={campsiteId} postComment={postComment} />
 . . .
                 </div>
             )
@@ -82,7 +82,7 @@ if ( props.campsite ) {
                 <RenderCampsite campsite={props.campsite} />
                 <RenderComments comments={props.comments}
                 comments={props.comments}
-                addComment={props.addComment}
+               postComment={props.postComment}
                 campsiteId={props.campsite.id}
                 />
             </div>
@@ -109,7 +109,7 @@ toggleModal() {
 }
 handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
 }
 
 
